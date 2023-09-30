a3d298bcf72195bc6ee92f233b49a550252a9854
Eugene Jung <114817519+jung-eugene@users.noreply.github.com>
2023-09-30 19:51:45 -0400

    update Todo.js

--- models/Todo.js
+++ models/Todo.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const TodoSchema = new mongoose.Schema({
-    content: {
-        type: String,
-        required: true,
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
+  content: {
+    type: String, 
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 })
 
-module.exports = new mongoose.model("Todo", TodoSchema)
+module.exports = new mongoose.model("Todo", TodoSchema);
