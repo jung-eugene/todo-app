fe0f04448b81be1146e2922e72670b8f481cd3e5
Eugene Jung <114817519+jung-eugene@users.noreply.github.com>
2023-09-30 19:50:39 -0400

    update index.js with comment lines

--- index.js
+++ index.js
@@ -6,61 +6,66 @@ const Todo = require("./models/Todo")
 
 dotenv.config();
 
+// connect to database
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+  console.log("Connected to db!");
+  // server configuration
+  app.listen(3000, () => console.log("Server started listening on port: 3000"));
+});
+
+// access css
+app.use("/static", express.static("public"));
+// allows to extract data from form
+app.use(express.urlencoded({ extended: true }));
+
+// view engine configuration
 app.set("view engine", "ejs");
 
-app.use("/static", express.static("public"));
-app.use(express.urlencoded({extended: true}));
-
-app.get('/', (req, res) => {
-    Todo.find({}, (err, tasks) => {
-        res.render('todo.ejs', {todoTasks: tasks});
-    });
-});
-
+// POST method
+// click add button -> app inserts data into db
 app.post('/', async (req, res) => {
-    const todoTask = new Todo ({
-        content: req.body.content
-    });
-    
-    try {
-        await todoTask.save();
-        res.redirect("/");
-    } catch (err) {
-        res.redirect("/");
-    }
+  const todoTask = new Todo ({
+  content: req.body.content
+  });
+
+  try {
+    await todoTask.save();
+    res.redirect("/");
+  } catch (err) {
+    res.redirect("/");
+  }
 });
 
-mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true}, () => {
-    console.log("Connected to database!");
-    app.listen(3000, () => console.log("Server up and running!"));
+// GET method
+// read data from database - enter page/add new item -> shown on app
+app.get("/", (req, res) => {
+  Todo.find({}, (err, tasks) => {
+    res.render("todo.ejs", { todoTasks: tasks });
+  });
 });
 
+// use findByIdAndUpdate to update tasks
 app
-.route("/edit/:id")
-.get((req, res) => {
+  .route("/edit/:id")
+  .get((req, res) => {
     const id = req.params.id;
-    Todo.find( {}, (err, tasks) => {
-        res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id});
+    Todo.find({}, (err, tasks) => {
+      res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
     });
-})
-
-.post((req, res) => {
+  })
+  .post((req, res) => {
     const id = req.params.id;
-    Todo.findByIdAndUpdate(id, { content: req.body.content}, err => {
-        if (err) return res.send(500, err);
-        res.redirect("/");
-    })
-})
+    Todo.findByIdAndUpdate(id, { content: req.body.content }, err => {
+    if (err) return res.send(500, err);
+      res.redirect("/");
+    });
+});
 
-app
-.route("/remove/:id").get((req, res) => {
-    const id = req.params.id;
-    Todo.findByIdAndRemove(id, err => {
-        if (err) return res.send(500, err);
-        res.redirect("/");
-    })
-})
-
-
-
-
+// use findByIdAndRemove to delete tasks
+app.route("/remove/:id").get((req, res) => {
+  const id = req.params.id;
+  Todo.findByIdAndRemove(id, err => {
+  if (err) return res.send(500, err);
+    res.redirect("/");
+  });
+});
